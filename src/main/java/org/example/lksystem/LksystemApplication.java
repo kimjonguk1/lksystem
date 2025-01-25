69d9c3e62bb57fd3be0b6fdb54d207a304d02b0d
kimjonguk1 <148532342+kimjonguk1@users.noreply.github.com>
2025-01-25 21:50:53 +0900

    add springboot project

---
+++ src/main/java/org/example/lksystem/LksystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.lksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LksystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LksystemApplication.class, args);
+	}
+
+}
