2e2056240289043886feb4baea7580bcd17afa23
kimjonguk1 <148532342+kimjonguk1@users.noreply.github.com>
2025-01-25 22:36:38 +0900

    홈 화면 구성

---
+++ src/main/java/org/example/lksystem/controllers/home/HomeController.java
@@ -0,0 +1,18 @@
+package org.example.lksystem.controllers.home;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home/index");
+        return modelAndView;
+    }
+}
