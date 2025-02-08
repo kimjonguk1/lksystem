5cc04b494770de8685e9ebea55af65df495cd1b6
kimjonguk1 <kydmsqls@naver.com>
2025-02-08 13:27:02 +0900

    시공 사례 작업중

---
+++ src/main/resources/static/contact/scripts/contact.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var user = "lktsj00"; // 이메일 아이디 부분
+    var domain = "daum.net"; // 이메일 도메인 부분
+    var email = user + "@" + domain; // 이메일 주소 조합
+
+    var emailLink = document.createElement("a");
+    emailLink.href = "mailto:" + email;
+    emailLink.textContent = email;
+
+    document.getElementById("email-link").appendChild(emailLink);
+});
